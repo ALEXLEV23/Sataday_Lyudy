a4668df000a2f8fa1ff9d548a07add67a5892dd2
ALEX_ASA <aleksandr.asatiani@mail.ru>
2023-02-25 10:50:39 +0300

    Inicial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val likes = 10325
+
+    if (likes % 10 == 1 && likes % 100 != 11)
+        println("Понравилось $likes человеку")
+    else
+        println("Понравилось $likes людям")
+}
+
+//fun main() {
+//    val likes = 2
+//    if (likes % 100 == 11)
+//        println("Понравилось $likes людям")
+//    else if (likes == 1 || (likes > 11 && likes % 10 == 1))
+//        println("Понравилось $likes человеку")
+//    else
+//        println("Понравилось $likes людям")
+//}
